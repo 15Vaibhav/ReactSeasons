e6008f65ff21853caf99b4210dbad3c85b33bb4e
vaibhav <vaibhavpandey15@bitbucket.com>
2019-06-27 14:32:58 +0530

    Updates

---
+++ precache-manifest.1db3d4a62e11f3913f93ab5ebb8f4bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0b8ed636e7680e9e3359ee2d8a6858",
+    "url": "/ReactSeasons/index.html"
+  },
+  {
+    "revision": "a7a5281d958e95b5d2c4",
+    "url": "/ReactSeasons/static/css/main.7c8ce657.chunk.css"
+  },
+  {
+    "revision": "3a7d90cd94e549cddc79",
+    "url": "/ReactSeasons/static/js/2.bfa2ca9e.chunk.js"
+  },
+  {
+    "revision": "a7a5281d958e95b5d2c4",
+    "url": "/ReactSeasons/static/js/main.1e361b21.chunk.js"
+  },
+  {
+    "revision": "1acc8f20b90e8ec5c6ad",
+    "url": "/ReactSeasons/static/js/runtime~main.8ec3e7ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/reactWeather/precache-manifest.5bb288a533979b50bc969271682ee15a.js"
+  "/ReactSeasons/precache-manifest.1db3d4a62e11f3913f93ab5ebb8f4bd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactWeather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactSeasons/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
